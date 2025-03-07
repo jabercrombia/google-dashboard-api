931b8ba44f5e40729d21fe9aa51529f0341d774d
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-07 12:25:42 -0600

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -36,6 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <AuthWrapper>{children}</AuthWrapper>
       </body>
+      <GoogleAnalytics gaId={process.env.GOOGLE_TRACKIND_ID || ''} />
     </html>
   );
 }
