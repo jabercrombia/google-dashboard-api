13343a532339c6eec323790caed6ee84efcf01de
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-10 00:46:46 -0500

    ui modal

--- app/components/modalpassword.tsx
+++ app/components/modalpassword.tsx
@@ -10,7 +10,7 @@ type PasswordModalProps = {
 const PasswordModal: React.FC<PasswordModalProps> = ({ onPasswordSubmit }) => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen] = useState(true);
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
